cb4ac366d0611096a5c50b1170279cc8e1874f94
kssumin <201566@jnu.ac.kr>
2024-06-15 10:20:02 +0900

    chore : sonar cloud 테스트를 의한 코드

---
+++ src/test/java/com/jungle/board/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.jungle.board;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ExampleTest {
+    @DisplayName("sonar cloud 를 위한 test")
+    @Test
+    void testExample() {
+        assertTrue(true);
+    }
+}
