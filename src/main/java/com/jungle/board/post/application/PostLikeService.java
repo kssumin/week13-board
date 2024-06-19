afe42b3bf53599040fe4f525b4f0514cd2158965
kssumin <201566@jnu.ac.kr>
2024-06-20 02:42:47 +0900

    feat : lock 획득 실패 시 재시도

--- src/main/java/com/jungle/board/post/application/PostLikeService.java
+++ src/main/java/com/jungle/board/post/application/PostLikeService.java
@@ -9,6 +9,9 @@ import com.jungle.board.post.persistence.PostRepository;
 import com.jungle.board.post.presentation.dto.request.CreatePostLikeRequest;
 import com.jungle.board.post.presentation.dto.request.DeletePostLikeRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +26,10 @@ public class PostLikeService {
 	private final PostLikeValidator postLikeValidator;
 
 	@Transactional
+	@Retryable(
+			retryFor = {ObjectOptimisticLockingFailureException.class},
+			maxAttempts = 1000,
+			backoff = @Backoff(100))
 	public void like(Long memberId, CreatePostLikeRequest command) {
 		Post post = postRepository.getById(command.postId());
 		Member member = memberRepository.getById(memberId);
